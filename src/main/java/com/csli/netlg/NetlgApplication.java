f5c72a4153cfd7f9cf94bf77b67256fd0f7f3438
Celso Gomes <celsolics@gmail.com>
2018-08-23 19:01:36 -0300

    Criação do Projeto

---
+++ src/main/java/com/csli/netlg/NetlgApplication.java
@@ -0,0 +1,12 @@
+package com.csli.netlg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetlgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetlgApplication.class, args);
+	}
+}
